90929c8d27263f30c27ee67d3cab6f7f8c3b4cee
chuanze <beyond198825@163.com>
2017-08-23 20:33:03 +0800

    委托进阶

---
+++ DeleGateDemo/Alarm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleGateDemo
+{
+    /// <summary>
+    /// 警报器
+    /// </summary>
+    public class Alarm
+    {
+        public void MakeAlert(int param)
+        {
+            Console.WriteLine("Alarm:嘀嘀嘀,水已经{0}度了：", param);
+        }
+        /// <summary>
+        /// 符合c#语言规范得写法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void MakeAlert(Object sender,Heater.BoiledEventArgs e)
+        {
+            Heater heater = (Heater)sender;//这里是不是很熟悉
+            Console.WriteLine("Alarm:{0}-{1}:", heater.area, heater.type);
+            Console.WriteLine("Alarm:嘀嘀嘀，水已经{0}度了：", e.temperature);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ DeleGateDemo/Display.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleGateDemo
+{
+    public class Display
+    {
+        public static void ShowMsg(int param)
+        {
+            Console.WriteLine("Display:水已烧开，当温度：{0}度",param);
+        }
+        /// <summary>
+        /// 符合c#语言规范写法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void ShowMsg(Object sender, Heater.BoiledEventArgs e)
+        {
+            Heater heater = (Heater)sender;
+            Console.WriteLine("Display:{0}-{1}:", heater.area, heater.type);
+            Console.WriteLine("Display:水烧开了，当前温度：{0}度", e.temperature);
+        }
+    }
+}
